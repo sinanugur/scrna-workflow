984dac83720a16e63ced80e85d23d25186d16311
Sinan Uğur Umu <sinanugur@gmail.com>
2023-02-24 15:25:45 +0100

    fixes on how to deal with RDS files integration

--- workflow/scripts/scrna-top-marker-plot.R
+++ workflow/scripts/scrna-top-marker-plot.R
@@ -15,8 +15,8 @@ option_list <- list(
 opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
 
-#try({source("workflow/scripts/scrna-functions.R")},silent=TRUE)
-#try({source(paste0(system("python -c 'import os; import cellsnake; print(os.path.dirname(cellsnake.__file__))'", intern = TRUE),"/scrna/workflow/scripts/scrna-functions.R"))},silent=TRUE)
+# try({source("workflow/scripts/scrna-functions.R")},silent=TRUE)
+# try({source(paste0(system("python -c 'import os; import cellsnake; print(os.path.dirname(cellsnake.__file__))'", intern = TRUE),"/scrna/workflow/scripts/scrna-functions.R"))},silent=TRUE)
 
 if (is.null(opt$xlsx)) {
     optparse::print_help(opt_parser)
@@ -30,13 +30,27 @@ require(tidyverse)
 Positive_Features <- openxlsx::read.xlsx(opt$xlsx)
 
 
-Positive_Features %>% group_by(cluster) %>% slice_max(n=20,order_by = avg_log2FC) -> df
+Positive_Features %>%
+    group_by(cluster) %>%
+    slice_max(n = 20, order_by = avg_log2FC) -> df
 
-maxFC=(df %>% pull(avg_log2FC) %>% max()) + 1
+maxFC <- (df %>% pull(avg_log2FC) %>% max()) + 1
 
-df %>% dplyr::mutate(n=dense_rank(desc(avg_log2FC))) %>% ggplot(aes(x=n,y=avg_log2FC,label=gene)) + 
-geom_text(angle=75,size=3) + facet_wrap(~cluster,ncol=4) + ggthemes::theme_few()  + theme(strip.text = element_text(size=12)) + ylim(c(0,maxFC)) + coord_cartesian(clip = "off",expand = TRUE) + ggtitle("Top markers") -> p1
+df %>%
+    group_by(cluster) %>%
+    dplyr::mutate(n = dense_rank(dplyr::desc(avg_log2FC))) %>%
+    ggplot(aes(x = n, y = avg_log2FC, label = gene)) +
+    geom_text(angle = 75, size = 3) +
+    facet_wrap(~cluster, ncol = 4) +
+    ggthemes::theme_few() +
+    theme(strip.text = element_text(size = 12)) +
+    ylim(c(0, maxFC)) +
+    coord_cartesian(clip = "off", expand = TRUE) +
+    ggtitle("Top markers") -> p1
 
-Positive_Features %>% distinct(cluster) %>% pull %>% length() -> n
+Positive_Features %>%
+    distinct(cluster) %>%
+    pull() %>%
+    length() -> n
 
-ggsave(opt$output.plot,p1,height=(2.1+ceiling(n/4)*2.1),width=9.5,useDingbats = TRUE)
+ggsave(opt$output.plot, p1, height = (2.1 + ceiling(n / 4) * 2.1), width = 9.5, useDingbats = TRUE)
