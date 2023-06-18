b6cd17eb3e8c229aadf22273c99937c9969545df
Sinan Uğur Umu <sinanugur@gmail.com>
2023-06-18 13:17:37 +0200

    NA fix

--- workflow/scripts/scrna-microbiome-dimplot.R
+++ workflow/scripts/scrna-microbiome-dimplot.R
@@ -123,10 +123,13 @@ for (i in plotting_taxas) {
 dev.off()
 
 
+pdf(opt$tplot, width = 7.5, height = 8)
+try({
+  FeaturePlot(scrna, features = "Total log2-Expression (Microbiome)", pt.size = 0.1, reduction = opt$reduction.type) &
+    scale_color_continuous(type = "viridis", na.value = "gray96") -> p2
 
-p2 <- FeaturePlot(scrna, features = "Total log2-Expression (Microbiome)", pt.size = 0.1, reduction = opt$reduction.type) &
-  scale_color_continuous(type = "viridis", na.value = "gray96")
-
-(p2 / guide_area()) + plot_layout(heights = c(2.5, 1), widths = c(1, 0.6), guides = "collect") -> p2
-
-ggsave(plot = p2, filename = opt$tplot, width = 7.5, height = 8)
+  p2 <- (p2 / guide_area()) + plot_layout(heights = c(2.5, 1), widths = c(1, 0.6), guides = "collect")
+  print(p2)
+})
+dev.off()
+# ggsave(plot = p2, filename = opt$tplot, width = 7.5, height = 8)
